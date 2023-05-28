1ff2f2a2c1c0d7791d3abc8d51d2c7384c293f32
Thant Ko Zaw <thantmkou10@gmail.com>
2023-05-28 20:28:27 +0630

    Add validations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { GetUsersQuery, getUsersQuerySchema } from './schemas/get-users.schema';
+import { ZodValidationPipe } from 'src/common/zod-validation.pipe';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getAllUsers(@Query() query) {
+  getAllUsers(
+    @Query(new ZodValidationPipe(getUsersQuerySchema)) query: GetUsersQuery,
+  ) {
     return this.usersService.getAllUsers(query);
   }
 
