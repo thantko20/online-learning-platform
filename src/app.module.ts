904470b469b893080bd94b3fe7bb558669c3c64a
Thant Ko Zaw <thantmkou10@gmail.com>
2023-05-30 21:24:56 +0630

    Add minimal logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,19 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { UtilsModule } from './utils/utils.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 @Module({
   imports: [PrismaModule, UsersModule, AuthModule, UtilsModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const before = Date.now();
+    return next.handle().pipe(
+      tap(() => {
+        const request = context.switchToHttp().getRequest<Request>();
+        const response = context.switchToHttp().getResponse<Response>();
+
+        const after = Date.now();
+        const responseTime = `${after - before}ms`;
+
+        const originalUrl = request.originalUrl;
+        const statusCode = response.statusCode;
+
+        const logPayload = JSON.stringify({
+          timestamp: new Date().toISOString(),
+          statusCode,
+          responseTime,
+          baseUrl: originalUrl,
+        });
+
+        console.log(logPayload);
+      }),
+    );
+  }
+}
