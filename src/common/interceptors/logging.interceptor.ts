fc0e27b88a4864fa0ef1203718710af4251201d7
Thant Ko Zaw <thantmkou10@gmail.com>
2023-05-30 23:33:20 +0630

    Some weird shit

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -6,31 +6,52 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { finalize, tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const before = Date.now();
     return next.handle().pipe(
-      tap(() => {
-        const request = context.switchToHttp().getRequest<Request>();
-        const response = context.switchToHttp().getResponse<Response>();
+      tap({
+        complete: () => {
+          const request = context.switchToHttp().getRequest<Request>();
+          const response = context.switchToHttp().getResponse<Response>();
 
-        const after = Date.now();
-        const responseTime = `${after - before}ms`;
+          const after = Date.now();
+          const responseTime = `${after - before}ms`;
 
-        const originalUrl = request.originalUrl;
-        const statusCode = response.statusCode;
+          const originalUrl = request.originalUrl;
+          const statusCode = response.statusCode;
 
-        const logPayload = JSON.stringify({
-          timestamp: new Date().toISOString(),
-          statusCode,
-          responseTime,
-          baseUrl: originalUrl,
-        });
+          const logPayload = JSON.stringify({
+            timestamp: new Date().toISOString(),
+            statusCode,
+            responseTime,
+            baseUrl: originalUrl,
+          });
 
-        console.log(logPayload);
+          console.log(logPayload);
+        },
+        error: (err) => {
+          const request = context.switchToHttp().getRequest<Request>();
+          const response = context.switchToHttp().getResponse<Response>();
+
+          const after = Date.now();
+          const responseTime = `${after - before}ms`;
+
+          const originalUrl = request.originalUrl;
+          const statusCode = response.statusCode;
+
+          const logPayload = JSON.stringify({
+            timestamp: new Date().toISOString(),
+            statusCode,
+            responseTime,
+            baseUrl: originalUrl,
+          });
+
+          console.log(logPayload);
+        },
       }),
     );
   }
